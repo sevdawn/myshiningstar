4832c5a7de3d04f73fc17afb01ed566cd97ac784
Virginia Swift <vdswif@gmail.com>
2021-02-26 22:40:57 -0500

    added social media icons, updated README

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 Code Louisville Javascript 2021 project by Virginia Swift
 
+
 Pages created with help from https://websitesetup.org/bootstrap-tutorial-for-beginners/ 
 Flickr page created with help from https://nanogallery2.nanostudio.org/ 
-Contact form to Google Sheets accomplished with help from SheetDB https://sheetdb.io/
+Contact form to Google Sheets created with help from SheetDB https://sheetdb.io/
+Social media icons by https://fontawesome.com/
+(I like to credit my sources as much as possible)
 
 Project features list:
 
@@ -18,5 +21,5 @@ Calculate and display data based on an external factor (ex: get the current date
 Create a form and save the values (on click of Submit button) to an external file 
 You must show us or document where that information is being stored so we can confirm that it’s being saved/persisted
 (Upon filling out the form and clicking submit, a popup button shows saying, "HEY IT WORKED! Click OK to see the results
-on this nifty Google Sheet!' Clicking ok takes you to the Google Sheet that shows the data has indeed been saved
+on this nifty Google Sheet!' Clicking OK takes you to the Google Sheet that shows the data has indeed been saved
 There's even a link in the Google Sheet that takes you back to the main page) 
--- main.js
+++ main.js
@@ -1,122 +1,109 @@
-// Modal JS //
-
-$(document).ready(function(){
-    $("#more").click(function(){
-        $("#more-modal").modal("show");
-    });
-});
-
-
 // external JSON file JS //
 
+$(document).ready(function() {
+  $.getJSON("venues.json", function(data) {
 
-$(document).ready(function () {
-    $.getJSON("venues.json", function (data) {
+      var arrItems = [];
+      $.each(data, function(index, value) {
+          arrItems.push(value);
+      });
 
-        var arrItems = [];      
-        $.each(data, function (index, value) {
-            arrItems.push(value);       
-        });
 
-        
-        var col = [];
-        for (var i = 0; i < arrItems.length; i++) {
-            for (var key in arrItems[i]) {
-                if (col.indexOf(key) === -1) {
-                    col.push(key);
-                }
-            }
-        }
+      var col = [];
+      for (var i = 0; i < arrItems.length; i++) {
+          for (var key in arrItems[i]) {
+              if (col.indexOf(key) === -1) {
+                  col.push(key);
+              }
+          }
+      }
 
-        
-        var table = document.createElement("table");
+      var table = document.createElement("table");
 
-        
 
-        var tr = table.insertRow(-1);                   
 
-        for (var i = 0; i < col.length; i++) {
-            var th = document.createElement("th");      
-            th.innerHTML = col[i];
-            tr.appendChild(th);
-        }
+      var tr = table.insertRow(-1);
 
-       
-        for (var i = 0; i < arrItems.length; i++) {
+      for (var i = 0; i < col.length; i++) {
+          var th = document.createElement("th");
+          th.innerHTML = col[i];
+          tr.appendChild(th);
+      }
 
-            tr = table.insertRow(-1);
 
-            for (var j = 0; j < col.length; j++) {
-                var tabCell = tr.insertCell(-1);
-                tabCell.innerHTML = arrItems[i][col[j]];
-            }
-        }
+      for (var i = 0; i < arrItems.length; i++) {
 
-        var divContainer = document.getElementById("showData");
-        divContainer.innerHTML = "";
-        divContainer.appendChild(table);
-    });
+          tr = table.insertRow(-1);
+
+          for (var j = 0; j < col.length; j++) {
+              var tabCell = tr.insertCell(-1);
+              tabCell.innerHTML = arrItems[i][col[j]];
+          }
+      }
+
+      var divContainer = document.getElementById("showData");
+      divContainer.innerHTML = "";
+      divContainer.appendChild(table);
+  });
 });
 
 // countdown clock JS //
 
 function getTimeRemaining(endtime) {
-    const total = Date.parse(endtime) - Date.parse(new Date());
-    const seconds = Math.floor((total / 1000) % 60);
-    const minutes = Math.floor((total / 1000 / 60) % 60);
-    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-    const days = Math.floor(total / (1000 * 60 * 60 * 24));
-    
-    return {
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+  return {
       total,
       days,
       hours,
       minutes,
       seconds
-    };
-  }
-  
-  function initializeClock(id, endtime) {
-    const clock = document.getElementById(id);
-    const daysSpan = clock.querySelector('.days');
-    const hoursSpan = clock.querySelector('.hours');
-    const minutesSpan = clock.querySelector('.minutes');
-    const secondsSpan = clock.querySelector('.seconds');
-  
-    function updateClock() {
+  };
+}
+
+function initializeClock(id, endtime) {
+  const clock = document.getElementById(id);
+  const daysSpan = clock.querySelector('.days');
+  const hoursSpan = clock.querySelector('.hours');
+  const minutesSpan = clock.querySelector('.minutes');
+  const secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
       const t = getTimeRemaining(endtime);
-  
+
       daysSpan.innerHTML = t.days;
       hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
       minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
       secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-  
+
       if (t.total <= 0) {
-        clearInterval(timeinterval);
+          clearInterval(timeinterval);
       }
-    }
-  
-    updateClock();
-    const timeinterval = setInterval(updateClock, 1000);
   }
-  
-  const deadline = new Date(Date.parse('2021-06-19') + 15 * 24 * 60 * 60 * 1000);
-  initializeClock('clockdiv', deadline);
 
-  // HTML form JS //
+  updateClock();
+  const timeinterval = setInterval(updateClock, 1000);
+}
 
-  var form = document.getElementById('sheetdb-form');
-  form.addEventListener("submit", e => {
+const deadline = new Date(Date.parse('2021-06-19') + 15 * 24 * 60 * 60 * 1000);
+initializeClock('clockdiv', deadline);
+
+// HTML form JS //
+
+var form = document.getElementById('sheetdb-form');
+form.addEventListener("submit", e => {
   e.preventDefault();
   fetch(form.action, {
-	  method : "POST",
-	  body: new FormData(document.getElementById("sheetdb-form")),
+      method: "POST",
+      body: new FormData(document.getElementById("sheetdb-form")),
   }).then(
-	  response => response.json()
+      response => response.json()
   ).then((html) => {
-	alert('HEY IT WORKED! Click OK to see the results on this nifty Google Sheet!')
-	window.location.assign("https://docs.google.com/spreadsheets/d/14bzfUQ8J7P4q3028VyCbxwf88zKniMpvMkrPrhXXHE0/")
+      alert('HEY IT WORKED! Click OK to see the results on this nifty Google Sheet!')
+      window.location.assign("https://docs.google.com/spreadsheets/d/14bzfUQ8J7P4q3028VyCbxwf88zKniMpvMkrPrhXXHE0/")
   });
 });
-
-
