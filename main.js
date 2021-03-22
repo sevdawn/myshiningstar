099619dd8c8d6635d7b7f878876402ab172997c4
Virginia Swift <vdswif@gmail.com>
2021-03-22 14:00:49 -0400

    reworking social media links

--- main.js
+++ main.js
@@ -1,50 +1,25 @@
 // external JSON file JS //
 
-$(document).ready(function() {
-  $.getJSON("venues.json", function(data) {
-
-      var arrItems = [];
-      $.each(data, function(index, value) {
-          arrItems.push(value);
-      });
-
-
-      var col = [];
-      for (var i = 0; i < arrItems.length; i++) {
-          for (var key in arrItems[i]) {
-              if (col.indexOf(key) === -1) {
-                  col.push(key);
-              }
-          }
+function loadJSONDoc() {
+  var xmlhttp;
+  xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      var obj = JSON.parse(xmlhttp.responseText);
+      var output = "<table>";
+      for (var name in obj[0].prices[0]) {
+        output += "<tr><td>" + name + "</td>" + "<td>" + venue[0].city[0][date] + "</td></tr>";
       }
+      output += "</table>";
+      document.getElementById("myList").innerHTML = output;
+    }
+  }
+  xmlhttp.open("GET", "venues.json", true);
+  xmlhttp.send();
+}
 
-      var table = document.createElement("table");
-
-
-
-      var tr = table.insertRow(-1);
-
-      for (var i = 0; i < col.length; i++) {
-          var th = document.createElement("th");
-          th.innerHTML = col[i];
-          tr.appendChild(th);
-      }
-
-
-      for (var i = 0; i < arrItems.length; i++) {
-
-          tr = table.insertRow(-1);
-
-          for (var j = 0; j < col.length; j++) {
-              var tabCell = tr.insertCell(-1);
-              tabCell.innerHTML = arrItems[i][col[j]];
-          }
-      }
-
-      var divContainer = document.getElementById("showData");
-      divContainer.innerHTML = "";
-      divContainer.appendChild(table);
-  });
+document.addEventListener("DOMContentLoaded", function(event) {
+  loadJSONDoc();
 });
 
 // countdown clock JS //
@@ -55,13 +30,13 @@ function getTimeRemaining(endtime) {
   const minutes = Math.floor((total / 1000 / 60) % 60);
   const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
   const days = Math.floor(total / (1000 * 60 * 60 * 24));
-
+  
   return {
-      total,
-      days,
-      hours,
-      minutes,
-      seconds
+    total,
+    days,
+    hours,
+    minutes,
+    seconds
   };
 }
 
@@ -73,24 +48,24 @@ function initializeClock(id, endtime) {
   const secondsSpan = clock.querySelector('.seconds');
 
   function updateClock() {
-      const t = getTimeRemaining(endtime);
+    const t = getTimeRemaining(endtime);
 
-      daysSpan.innerHTML = t.days;
-      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
-      if (t.total <= 0) {
-          clearInterval(timeinterval);
-      }
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
   }
 
   updateClock();
   const timeinterval = setInterval(updateClock, 1000);
 }
-
-const deadline = new Date(Date.parse('2021-06-19') + 15 * 24 * 60 * 60 * 1000);
-initializeClock('clockdiv', deadline);
+  
+  const deadline = new Date(Date.parse('2021-06-19') + 15 * 24 * 60 * 60 * 1000);
+  initializeClock('clockdiv', deadline);
 
 // HTML form JS //
 
